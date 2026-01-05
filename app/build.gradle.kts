53a2ec613acef6b537bf6400aa57b8b6b5ecc86e
vinithreddybanda <vinithreddy08.123@gmail.com>
2026-01-05 19:01:50 +0530

    Update dependencies and migrate to version catalog
    * Move `core-splashscreen` version definition to version catalog and update implementation
    * Correct `androidx.compose.material3` dependency reference
    * Update library versions for `lifecycleViewmodelCompose`, `navigationCompose`, and `appcompat`
    * Remove unused versions and library definitions for `palette`, `media3-exoplayer`, and `media3-ui`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
@@ -56,7 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3.yml)
+    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.coil.compose)
     implementation(libs.coil.video)
