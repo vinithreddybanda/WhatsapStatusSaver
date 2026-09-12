b9f7bb00e57c53df3139ac940246ce3f8df0a581
Vinith Reddy Banda <116493221+vinithreddybanda@users.noreply.github.com>
2026-09-13 01:39:14 +0530

    Fix graphicsLayer import

--- app/src/main/java/com/vinithreddybanda/whatsapstatus/MainActivity.kt
+++ app/src/main/java/com/vinithreddybanda/whatsapstatus/MainActivity.kt
@@ -93,6 +93,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
